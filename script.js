bfa388830a880b56d3d93b3c0595369fc37dcc43
Leynad <Daniel@DC.local>
2017-10-29 23:41:49 +0100

    Création Navbar

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
+	
+	// Skill bar animation
 	$('.skillbar').each(function(){
 		$(this).find('.skillbar-bar').animate({
 			width:$(this).attr('data-percent')
-		},5000);
+		},9000);
 	});
+	
+	// Scroll animation
+	$('.navbar a').on('click', function() { // Au clic sur un élément
+		var page = $(this).attr('href'); // Page cible
+		var speed = 750; // Durée de l'animation (en ms)
+		$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+			return false;
+		});
 });
